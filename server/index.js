470cb2c0c4e46ea25136ed943415865f04b3a18e
NyatiZw <appsmaid19@gmail.com>
2023-12-20 04:52:17 +0000

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+// server/index.js
+
+
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+
+const PORT = 3001;
+
+app.use(express.static(path.join(__dirname, 'src/')));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/', 'index.html'));;
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+
+import "./App.css";
+
+
+function App() {
+	return (
+		<div>
+		  <h1>Hello Welcome to KeepSafe!</h1>
+		  <footer>By Simba Hanga 2023</footer>
+		</div>
+	);
+}
+
+
+export default App;
