b7064c72c0249ab170c1efb56c580796604e45bf
NyatiZw <appsmaid19@gmail.com>
2023-12-24 01:00:35 +0000

    updating UI

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,8 @@
 // src/Map.js
-import React, { useEffect } from 'react';
+
 import mapboxgl from 'mapbox-gl';
+import React, { useEffect } from 'react';
+//import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Map = () => {
 	useEffect(() => {
@@ -8,13 +10,19 @@ const Map = () => {
 
 		const map = new mapboxgl.Map({
 			container: 'map',
-			style: 'mapbox://styles/mapbox/streets-v11'
+			//style: 'mapbox://styles/mapbox/navigation-day-v1'
+			center: [27.9, -26],
+			zoom: 9,
+		});
+
+		map.on('style.load', () => {
+			    map.setConfigProperty('basemap', 'lightPreset', 'dusk', 'showPointsOfInterestLabels', true, 'showTransitLabels', true);
 		});
 
 		return () => map.remove();
 	}, []);
 
-	return <div id="map" style={{ width: '100%', height: '400px' }} />;
+	return <div id="map" style={{ width: '100%', height: '100vh' }} />;
 };
 
 
